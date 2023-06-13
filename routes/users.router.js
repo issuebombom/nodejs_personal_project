500ad3e469fe60286eb05a7395843b59d32c777f
issuebombom <issuebombom@gmail.com>
2023-06-13 12:47:26 +0900

    feat: only new user can sign-up

--- routes/users.router.js
+++ routes/users.router.js
@@ -3,17 +3,25 @@ const usersRouter = express.Router();
 
 const User = require('../schemas/user');
 
+// 유저 조회(API 확인용)
 usersRouter.get('/', async (req, res) => {
   const getUsers = await User.find({});
 
-  res.json({ users: getUsers });
+  res.send({ users: getUsers });
 });
 
+// sign-up
 usersRouter.post('/', async (req, res) => {
   const { userId, password } = req.body;
 
-  const createdUser = await User.create({ userId, password });
+  // 고유값에 대한 검증을 합니다.
+  const findUser = await User.find({ userId });
+  if (findUser.length !== 0) {
+    res.sendStatus(400);
+  }
 
+  // 계정 생성
+  const createdUser = await User.create({ userId, password });
   res.send({ msg: '유저 등록 완료' });
 });
 
