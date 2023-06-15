e13a49191e743c5e7cc6f87201fcb81ac829a20d
issuebombom <issuebombom@gmail.com>
2023-06-15 16:28:31 +0900

    fix: apply verify middleware to routers

--- routes/authorization.router.js
+++ routes/authorization.router.js
@@ -1,16 +1,8 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
-
-const User = require('../schemas/user');
-const authMiddleware = require('../verification'); // 토큰 검증을 위한 미들웨어
 
 const authorizationController = require('../controllers/authorization.controller');
-
 const authorizationRouter = express.Router();
-authorizationRouter.use(cookieParser());
 
 authorizationRouter.post('/login', authorizationController.login);
-
-authorizationRouter.get('/refresh', authMiddleware, authorizationController.refresh);
 
 module.exports = authorizationRouter;
--- routes/posts.router.js
+++ routes/posts.router.js
@@ -1,53 +1,66 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const postsController = require('../controllers/posts.controller');
 const commentsController = require('../controllers/comments.controller');
-const authMiddleware = require('../verification'); // 토큰 검증을 위한 미들웨어
+const { verifyAccessToken, replaceAccessToken } = require('../verification'); // 토큰 검증을 위한 미들웨어
 
 const postsRouter = express.Router();
-
+postsRouter.use(cookieParser());
 
 // 전체 포스트 확인(공개)
 postsRouter.get('/', postsController.getPosts);
 
 // 포스트 작성
-postsRouter.post('/', authMiddleware, postsController.writePosts);
+postsRouter.post('/', verifyAccessToken, replaceAccessToken, postsController.writePosts);
 
 // 내 게시글 수정 클릭 -> 비밀번호 확인 사이트로 이동(href에 게시글 id 전달) -> 비밀번호 검증 -> 수정페이지에서 수정
 // 비밀번호 검증 페이지(쿼리값 필요)
 postsRouter.post(
   '/:postId/password-verification',
-  authMiddleware,
+  verifyAccessToken,
   postsController.passwordVerificationForPosts
 );
 
 // 수정페이지에서 수정완료(올리기) 클릭
-postsRouter.put('/:postId', authMiddleware, postsController.editPosts);
+postsRouter.put('/:postId', verifyAccessToken, replaceAccessToken, postsController.editPosts);
 
 // 포스트 삭제하기
-postsRouter.delete('/:postId', authMiddleware, postsController.deletePosts);
+postsRouter.delete('/:postId', verifyAccessToken, replaceAccessToken, postsController.deletePosts);
 
 // 포스트 댓글 확인(공개)
 postsRouter.get('/:postId/comments', commentsController.getComments);
 
 // 포스트 댓글 작성(회원 전용)
-postsRouter.post('/:postId/comments', authMiddleware, commentsController.writeComments);
+postsRouter.post(
+  '/:postId/comments',
+  verifyAccessToken,
+  replaceAccessToken,
+  commentsController.writeComments
+);
 
 // 내 댓글 수정 클릭 -> 비밀번호 확인 사이트로 이동(href에 게시글 id 전달) -> 비밀번호 검증 -> 수정페이지에서 수정
 // 비밀번호 검증 페이지(쿼리값 필요)
 postsRouter.post(
   '/:postId/comments/:commentId/password-verification',
-  authMiddleware,
+  verifyAccessToken,
+  replaceAccessToken,
   commentsController.passwordVerificationForComments
 );
 
 // 수정페이지에서 수정완료(올리기) 클릭
-postsRouter.put('/:postId/comments/:commentId', authMiddleware, commentsController.editComments);
+postsRouter.put(
+  '/:postId/comments/:commentId',
+  verifyAccessToken,
+  replaceAccessToken,
+  commentsController.editComments
+);
 
 // 댓글 삭제하기
 postsRouter.delete(
   '/:postId/comments/:commentId',
-  authMiddleware,
+  verifyAccessToken,
+  replaceAccessToken,
   commentsController.deleteComments
 );
 
