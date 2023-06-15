d05bc9534fd9790a816277f78b2f0ab041fdde70
issuebombom <issuebombom@gmail.com>
2023-06-15 16:25:36 +0900

    refactor: make generate token func. using closure

--- controllers/authorization.controller.js
+++ controllers/authorization.controller.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const User = require('../schemas/user');
+const { getAccessToken, getRefreshToken } = require('../verification'); // 토큰 생성기
 
 const login = async (req, res) => {
   const { username } = req.body;
@@ -9,59 +10,26 @@ const login = async (req, res) => {
     const findUser = await User.findOne({ username });
     if (!findUser) return res.status(401).send({ msg: '회원이 아닙니다.' });
 
-    // 토큰 생성
-    const accessToken = jwt.sign({ username, _id: findUser._id }, process.env.ACCESS_TOKEN_KEY, {
-      expiresIn: '30m',
-    });
-    const refreshToken = jwt.sign({ username, _id: findUser._id }, process.env.REFRESH_TOKEN_KEY, {
-      expiresIn: '1d',
-    });
+    // 토큰을 보내기 위해 만든 함수
+    async function sendRefreshToken(refreshToken) {
+      // refresh token 등록
+      const update = { $set: { refreshToken } };
+      await User.updateOne(findUser, update);
 
-    // refresh token 등록
-    const update = { $set: { refreshToken } };
-    await User.updateOne(findUser, update);
+      // refresh token 쿠키로 전달
+      res.cookie('issuebombomCookie', refreshToken, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000, // 24 시간
+      });
+    }
 
-    // refresh token 쿠키로 전달
-    res.cookie('issuebombomCookie', refreshToken, {
-      httpOnly: true,
-      maxAge: 24 * 60 * 60 * 1000, // 24 시간
-    });
+    // 리프레시 토큰을 쿠키와 데이터베이스에 전달
+    await sendRefreshToken(getRefreshToken(username, findUser.id));
 
-    res.setHeader('Authorization', `Bearer ${accessToken}`);
+    // 엑세스 토큰을 headers의 Authorization으로 보냄
+    res.setHeader('Authorization', `Bearer ${getAccessToken(username, findUser._id)}`);
+
     res.status(200).send({ msg: '로그인 완료' });
-  } catch (err) {
-    console.error(err.name, ':', err.message);
-    return res.status(500).send({ msg: `${err.message}` });
-  }
-};
-
-// 리프레시 토큰으로 엑세스 토큰 발행
-const refresh = async (req, res) => {
-  try {
-    const cookies = req.cookies;
-    // 쿠키가 없는 경우
-    if (!cookies?.issuebombomCookie) return res.status(403).send({ msg: '찾는 쿠키 없음' });
-
-    // 쿠키가 있으면
-    const refreshToken = cookies.issuebombomCookie;
-    // DB에 저장된 쿠키가 있는지 확인
-    const user = await User.findOne({ refreshToken });
-    if (!user) return res.status(403).send({ msg: '등록된 리프레시 토큰이 없음' });
-    // 쿠키 검증
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_KEY, (err, user) => {
-      // refresh token이 만료된 경우 재로그인 안내
-      if (err) return res.status(403).send({ msg: '리프레시 토큰이 만료됨 (재 로그인 필요)' });
-
-      // 신규 토큰 생성
-      const accessToken = jwt.sign(
-        { username: user.username, _id: user._id }, // 현재 user에는 토큰의 iat와 exp가 담겨있어 제외해야 한다.
-        process.env.ACCESS_TOKEN_KEY,
-        { expiresIn: '30m' }
-      );
-      // 재발급
-      res.setHeader('Authorization', `Bearer ${accessToken}`);
-      res.status(200).send({ msg: '토큰 재발급 완료' });
-    });
   } catch (err) {
     console.error(err.name, ':', err.message);
     return res.status(500).send({ msg: `${err.message}` });
@@ -70,5 +38,4 @@ const refresh = async (req, res) => {
 
 module.exports = {
   login,
-  refresh,
 };
--- verification.js
+++ verification.js
@@ -1,20 +1,76 @@
 const jwt = require('jsonwebtoken');
-// const cookieParser = require('cookie-parser');
+const User = require('./schemas/user');
 
-// 토큰 검증을 위한 미들웨어
-function authMiddleware(req, res, next) {
+// 엑세스 토큰 생성기
+const getAccessToken = ((username, _id) => {
+  const accessToken = jwt.sign({ username, _id }, process.env.ACCESS_TOKEN_KEY, {
+    expiresIn: '30m',
+  });
+  return () => accessToken;
+})();
+
+// 리프레시 토큰 생성기
+const getRefreshToken = ((username, _id) => {
+  const refreshToken = jwt.sign({ username, _id }, process.env.REFRESH_TOKEN_KEY, {
+    expiresIn: '30m',
+  });
+  return () => refreshToken;
+})();
+
+// 엑세스 토큰 검증을 위한 미들웨어
+function verifyAccessToken(req, res, next) {
   // auth에서 access token을 획득합니다.
   const authHeader = req.headers.authorization;
   const token = authHeader && authHeader.split(' ')[1]; // Bearer 제거
-  if (token == null) return res.status(401).send({ msg: '엑세스 토큰을 입력해 주세요.' }); // 토큰이 없다면 종료
+  if (!token) return res.status(401).send({ msg: '엑세스 토큰을 입력해 주세요.' }); // 토큰이 없다면 종료
 
   // access token 검증
   jwt.verify(token, process.env.ACCESS_TOKEN_KEY, (err, user) => {
     // access token이 만료된 경우 재생성하기
-    if (err) return res.status(403).send({ msg: '엑세스 토큰이 만료되었습니다.' });
+    if (err) {
+      req.expired = true;
+      console.error(err.name, ':', err.message);
+    }
     req.user = user;
     next();
   });
 }
 
-module.exports = authMiddleware;
+// 엑세스 토큰 만료 시 재발급을 위한 미들웨어
+async function replaceAccessToken(req, res, next) {
+  if (req.expired) {
+    try {
+      const cookies = req.cookies;
+      // 쿠키가 없는 경우
+      if (!cookies?.issuebombomCookie)
+        return res.status(403).send({ msg: '엑세스 토큰 재발급을 위한 쿠키 없음' });
+
+      // 쿠키가 있으면
+      const refreshToken = cookies.issuebombomCookie;
+      // DB에 저장된 쿠키가 있는지 확인
+      const user = await User.findOne({ refreshToken });
+      if (!user) return res.status(403).send({ msg: '해당 쿠키에는 등록된 리프레시 토큰이 없음' });
+      // 쿠키 검증
+      jwt.verify(refreshToken, process.env.REFRESH_TOKEN_KEY, (err, user) => {
+        // refresh token이 만료된 경우 재로그인 안내
+        if (err) return res.status(403).send({ msg: '리프레시 토큰이 만료됨 (재 로그인 필요)' });
+
+        // 신규 토큰 생성 및 재발급
+        res.setHeader('Authorization', `Bearer ${getAccessToken(user.username, user._id)}`);
+        res.status(200).send({ msg: '엑세스 토큰이 만료되어 재발급' });
+      });
+    } catch (err) {
+      console.error(err.name, ':', err.message);
+      return res.status(500).send({ msg: `${err.message}` });
+    }
+  } else {
+    next();
+  }
+}
+
+module.exports = {
+  getAccessToken,
+  getRefreshToken,
+  verifyAccessToken,
+  replaceAccessToken,
+};
